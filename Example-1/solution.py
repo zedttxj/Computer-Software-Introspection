b6256741795ea3306acdc3e747ab6399604f7b0d
Nguyen Anh Minh <anhminh.nguyen@ace.tamut.edu>
2025-02-18 12:32:18 -0600

    Update solution.py

--- Example-1/solution.py
+++ Example-1/solution.py
@@ -6,12 +6,12 @@ for i in range(0x738):
     if dat[i*24+19] == 10: continue
     data.append(dat[i*24+19])
     color += [dat[i*24+7:i*24+18].split(b";")]
-data = bytes(data)
-colors = []
+colors = [] # colors of the pixel
 for i in color:
     colors += [bytes([int(j) for j in i])]
 # print(colors)
-def sqr(tmp, w, h):
+# print(len(colors), len(data))
+def rec(tmp, w, h): # function that breaks the image into rectangles
     sz = []
     szt = []
     cnt = True
@@ -38,28 +38,16 @@ def sqr(tmp, w, h):
                 sz.append([j,i,lw-j,kh-i])
                 szt.append((kh-i)*(lw-j))
     return sz, szt
-sz, szt = sqr(list(data),0x4c,0x18)
-ls = []
-with open('output.cimg','wb') as output:
-    str = b"cIMG\x03\x00\x4c\x18" + bytes([len(sz)*2]) + b"\x00\x00\x00"
-    #str += b"\x02\x00\x00\x00\x01\x01\xff\xff\xff\x7e"
-    str = b""
-    cnt = 0
+sz, szt = rec(list(data),0x4c,0x18)
+with open('./output.cimg','wb') as output:
+    str = b"cIMG\x03\x00\x4c\x18" # 0x4c is the width and 0x18 is the height according to the desired output
+    str += len(sz).to_bytes(4, byteorder='big')[::-1] # According to the number of directives used
     for i in range(len(sz)):
-        #str += b"\x02\x00"+bytes(sz[i])
-        #str += b"\x03\x00"+bytes([i])+bytes(sz[i][2:])
-        tmp = b""
-        for h in range(sz[i][1],sz[i][1]+sz[i][3]):
-            for w in range(sz[i][0],sz[i][0]+sz[i][2]):
-                tmp += bytes([data[h*0x4c+w]])
-                #str += colors[h*0x4c+w] + bytes([data[h*0x4c+w]])
-        if not(tmp in ls):
-            ls.append(tmp)
-            cnt += 1
-            str += b"\x03\x00"+bytes([ls.index(tmp)])+bytes(sz[i][2:])+tmp
-        #str += b"\x04\x00"+bytes([ls.index(tmp)])+colors[sz[i][0]+sz[i][1]*0x4c]+bytes(sz[i][:2])
-        str += b"\x04\x00"+bytes([ls.index(tmp)])+(ls.index(tmp)*500000).to_bytes(3, byteorder='big')+bytes(sz[i][:2])
-        cnt += 1
-    str = b"cIMG\x03\x00\x4c\x18" + bytes([cnt]) + b"\x00\x00\x00" + str
-    print(len(str), cnt)
+        if data[i]!=ord(' '): # filtering out the empty space characters
+            str += b"\x02\x00" # calling directive 2, which is handle_2
+            str += bytes(sz[i]) # the position where we put the pixel and the size of the rectangle
+            for h in range(sz[i][1],sz[i][1]+sz[i][3]):
+                for w in range(sz[i][0],sz[i][0]+sz[i][2]):
+                    str += colors[h*0x4c+w] + bytes([data[h*0x4c+w]]) # the actual data of the pixel
+    print(len(str))
     output.write(str)
